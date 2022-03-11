246b0f8bf03d123d9cb4a824f6844aa33e8ff29f
Meziane270 <moulmas@myges.fr>
2022-03-11 21:07:19 +0100

    project initial commit

---
+++ lib/entities/User.dart
@@ -0,0 +1,11 @@
+
+class User {
+  final String id;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String password;
+
+  User(this.id, this.firstName, this.lastName, this.email, this.password);
+
+}
